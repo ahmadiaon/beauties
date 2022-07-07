b8405932322b47fa73e20ff9a14f01ce57a808f8
ahmadiaon <dataahmadi2020@gmail.com>
2022-07-07 15:20:08 +0700

    beauties

---
+++ env.php
@@ -0,0 +1,14 @@
+<?php
+// database
+define("HOST", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DATABASE", "beautiesdb");
+define("APP_NAME", "beauties");
+define("APP_URL", "./");
+define("CITY", "Palangka Raya");
+define("POSTAL_CODE", "73112");
+
+
+
+?>
